ddb93f4259650382e22b90e110ddd14d2bbb59fa
ngrachik <ngrachik2011@gmail.com>
2022-02-07 16:55:44 +0300

    new file:   dist/favicon.ico 	new file:   dist/img/icons/android-chrome-192x192.png 	new file:   dist/img/icons/android-chrome-512x512.png 	new file:   dist/img/icons/android-chrome-maskable-192x192.png 	new file:   dist/img/icons/android-chrome-maskable-512x512.png 	new file:   dist/img/icons/apple-touch-icon-120x120.png 	new file:   dist/img/icons/apple-touch-icon-152x152.png 	new file:   dist/img/icons/apple-touch-icon-180x180.png 	new file:   dist/img/icons/apple-touch-icon-60x60.png 	new file:   dist/img/icons/apple-touch-icon-76x76.png 	new file:   dist/img/icons/apple-touch-icon.png 	new file:   dist/img/icons/favicon-16x16.png 	new file:   dist/img/icons/favicon-32x32.png 	new file:   dist/img/icons/msapplication-icon-144x144.png 	new file:   dist/img/icons/mstile-150x150.png 	new file:   dist/img/icons/safari-pinned-tab.svg 	new file:   dist/index.html 	new file:   dist/js/app.8e75945c.js 	new file:   dist/js/app.8e75945c.js.map 	new file:   dist/js/chunk-vendors.b53b4d9f.js 	new file:   dist/js/chunk-vendors.b53b4d9f.js.map 	new file:   dist/manifest.json 	new file:   dist/precache-manifest.c9f87f26399ea3b787c970f19456bd05.js 	new file:   dist/robots.txt 	new file:   dist/service-worker.js

---
+++ starting-template/dist/precache-manifest.c9f87f26399ea3b787c970f19456bd05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1900f1c48da41fd058eae1e0bb452edf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85e61ef4cf6fac5236c1",
+    "url": "/js/app.8e75945c.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
